e0244924949e6cf1d30e2afef5fcd8d9ca533e66
Leonardo Fuso Nuzzo <leonardo.nuzzo@ifood.com.br>
2022-06-16 17:07:55 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### C Vector Distance Study
+### Distance __m256d
+
+I've built this lib to benchmark different distance measures operations using some ___m256d_ vector functions.
+
+## Build process
+
+``` bash
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
+
---
+++ distance__m256d.h
@@ -0,0 +1,11 @@
+#ifndef DISTANCE__M256D_DISTANCE__M256D_H
+#define DISTANCE__M256D_DISTANCE__M256D_H
+
+#include <immintrin.h>
+#include <math.h>
+
+double cosine(const double *, const double *, unsigned long);
+double euclidean(const double *, const double *, unsigned long);
+double manhattan(const double *, const double *, unsigned long);
+
+#endif //DISTANCE__M256D_DISTANCE__M256D_H
